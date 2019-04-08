e6f4942abd17589dc8507bd3faf216442d1db1c7
whv2li8 <wim.holvoet@colruytgroup.com>
2019-04-08 10:02:06 +0200

    wip

--- app/api/src/main.js
+++ app/api/src/main.js
@@ -14,7 +14,13 @@ function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = yield core_1.NestFactory.create(app_module_1.AppModule);
         app.setGlobalPrefix('api');
-        app.enableCors({ origin: '*' });
+        app.enableCors();
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+            next();
+        });
         yield app.listen(3000);
     });
 }
